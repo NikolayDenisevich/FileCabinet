8fcd9fd1fec3fc4ce1df15dbfc86621cedcb0900
Nikolay Denisevich <n_denisevich@inbox.ru>
2020-09-15 22:18:57 +0300

    add-search

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace FileCabinetApp
 {
     public class FileCabinetService
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> lastNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> dateOfBirthDictionary = new Dictionary<string, List<FileCabinetRecord>>();
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short zipCode, string city, string street, decimal salary, char gender)
         {
+            this.ValidateArgumets(firstName, lastName, dateOfBirth, zipCode, city, street, salary, gender);
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
@@ -23,9 +28,44 @@ namespace FileCabinetApp
             };
 
             this.list.Add(record);
+            this.AddRecordToDictionary(firstName, record, this.firstNameDictionary);
+            this.AddRecordToDictionary(lastName, record, this.lastNameDictionary);
+            this.AddRecordToDictionary(dateOfBirth.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo), record, this.dateOfBirthDictionary);
 
             return record.Id;
         }
+
+        public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short zipCode, string city, string street, decimal salary, char gender)
+        {
+            this.ValidateArgumets(firstName, lastName, dateOfBirth, zipCode, city, street, salary, gender);
+            FileCabinetRecord listRecord = this.list.Find(i => i.Id == id);
+            if (listRecord is null)
+            {
+                throw new ArgumentException($"There is no record #{id} in the list.");
+            }
+
+            string oldFirstName = listRecord.FirstName;
+            string oldLastName = listRecord.LastName;
+            string oldDateOfBirth = listRecord.DateOfBirth.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo);
+            listRecord.FirstName = firstName;
+            listRecord.LastName = lastName;
+            listRecord.DateOfBirth = dateOfBirth;
+            listRecord.ZipCode = zipCode;
+            listRecord.City = city;
+            listRecord.Street = street;
+            listRecord.Salary = salary;
+            listRecord.Gender = gender;
+
+            this.EditRecordInDictionary(oldFirstName, firstName, id, listRecord, this.firstNameDictionary);
+            this.EditRecordInDictionary(oldLastName, lastName, id, listRecord, this.lastNameDictionary);
+            this.EditRecordInDictionary(oldDateOfBirth, dateOfBirth.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo), id, listRecord, this.dateOfBirthDictionary);
+        }
+
+        public FileCabinetRecord[] FindByFirstName(string firstName) => this.GetRecordsFromDictionary(firstName, this.firstNameDictionary);
+
+        public FileCabinetRecord[] FindByLastName(string lastName) => this.GetRecordsFromDictionary(lastName, this.lastNameDictionary);
+
+        public FileCabinetRecord[] FindByDateOfBirth(DateTime date) => this.GetRecordsFromDictionary(date.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo), this.dateOfBirthDictionary);
 
         public FileCabinetRecord[] GetRecords()
         {
@@ -35,6 +75,127 @@ namespace FileCabinetApp
         public int GetStat()
         {
             return this.list.Count;
+        }
+
+        private void ValidateArgumets(string firstName, string lastName, DateTime dateOfBirth, short zipCode, string city, string street, decimal salary, char gender)
+        {
+            if (firstName is null)
+            {
+                throw new ArgumentNullException($"{nameof(firstName)} is null");
+            }
+
+            string trimmedFirsName = firstName.Trim();
+
+            if (trimmedFirsName.Length < 2 || trimmedFirsName.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(firstName)}.Length should be from 2 to 60. {nameof(firstName)} should not consist only of white-spaces characters");
+            }
+
+            if (lastName is null)
+            {
+                throw new ArgumentNullException($"{nameof(lastName)} is null");
+            }
+
+            string trimmedLastName = lastName.Trim();
+
+            if (trimmedLastName.Length < 2 || trimmedLastName.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(lastName)}.Length should be from 2 to 60. {nameof(lastName)} should not consist only of white-spaces characters");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth >= DateTime.Now)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} should be more than 01-Jan-1950 and not more than {DateTime.Now.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo)} or invalid date format input");
+            }
+
+            if (zipCode < 1 || zipCode > 9999)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(zipCode)} range is 1..9999");
+            }
+
+            if (city is null)
+            {
+                throw new ArgumentNullException($"{nameof(city)} is null");
+            }
+
+            string trimmedCity = city.Trim();
+
+            if (trimmedCity.Length < 2 || trimmedCity.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(city)}.Length should be from 2 to 60. {nameof(city)} should not consist only of white-spaces characters");
+            }
+
+            if (street is null)
+            {
+                throw new ArgumentNullException($"{nameof(street)} is null");
+            }
+
+            string trimmedStreet = street.Trim();
+
+            if (trimmedStreet.Length < 2 || trimmedStreet.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(street)}.Length should be from 2 to 60. {nameof(street)} should not consist only of white-spaces characters");
+            }
+
+            if (salary < 0 || salary > 100000)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(salary)} range is 0..100 000");
+            }
+
+            if (gender != 'm' && gender != 'M' && gender != 'f' && gender != 'F')
+            {
+                throw new ArgumentException($"{nameof(gender)} permissible values are :'m', 'M', 'f', 'F'.");
+            }
+        }
+
+        private FileCabinetRecord[] GetRecordsFromDictionary(string key, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            List<FileCabinetRecord> valuesList;
+            dictionary.TryGetValue(key.ToUpperInvariant(), out valuesList);
+            return (valuesList is null) ? Array.Empty<FileCabinetRecord>() : valuesList.ToArray();
+        }
+
+        private void EditRecordInDictionary(string oldName, string newName, int id, FileCabinetRecord record, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            List<FileCabinetRecord> valuesList;
+            if (oldName.Equals(newName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                dictionary.TryGetValue(oldName.ToUpperInvariant(), out valuesList);
+                FileCabinetRecord dictionaryRecord = valuesList.Find(i => i.Id == id);
+                dictionaryRecord = record;
+            }
+            else
+            {
+                dictionary.TryGetValue(oldName.ToUpperInvariant(), out valuesList);
+                FileCabinetRecord oldDictionaryRecord = valuesList.Find(i => i.Id == id);
+                valuesList.Remove(oldDictionaryRecord);
+            }
+
+            if (dictionary.TryGetValue(newName.ToUpperInvariant(), out valuesList))
+            {
+                valuesList.Add(record);
+            }
+            else
+            {
+                valuesList = new List<FileCabinetRecord>();
+                valuesList.Add(record);
+                dictionary.Add(newName.ToUpperInvariant(), valuesList);
+            }
+        }
+
+        private void AddRecordToDictionary(string name, FileCabinetRecord record, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            List<FileCabinetRecord> valuesList;
+            if (dictionary.TryGetValue(name.ToUpperInvariant(), out valuesList))
+            {
+                valuesList.Add(record);
+            }
+            else
+            {
+                valuesList = new List<FileCabinetRecord>();
+                valuesList.Add(record);
+                dictionary.Add(name.ToUpperInvariant(), valuesList);
+            }
         }
     }
 }
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -21,6 +21,15 @@ namespace FileCabinetApp
             new Tuple<string, Action<string>>("stat", Stat),
             new Tuple<string, Action<string>>("create", Create),
             new Tuple<string, Action<string>>("list", List),
+            new Tuple<string, Action<string>>("edit", Edit),
+            new Tuple<string, Action<string>>("find", Find),
+        };
+
+        private static Tuple<string, string, Action<string, string>>[] properties = new Tuple<string, string, Action<string, string>>[]
+        {
+            new Tuple<string, string, Action<string, string>>("firstname", "First Name", ShowByFirstName),
+            new Tuple<string, string, Action<string, string>>("lastname", "Last Name", ShowByLastName),
+            new Tuple<string, string, Action<string, string>>("dateofbirth", "Date of birth", ShowByDate),
         };
 
         private static string[][] helpMessages = new string[][]
@@ -28,8 +37,10 @@ namespace FileCabinetApp
             new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
             new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
             new string[] { "stat", "displays statistics on records", "The 'stat' command displays statistics on records." },
-            new string[] { "create", "creates a new record", "The 'stat' creates a new record." },
+            new string[] { "create", "creates a new record", "The 'create' creates a new record." },
             new string[] { "list", "displays a list of records added to the service.", "The 'list' displays a list of records added to the service." },
+            new string[] { "edit", "edits an existing record.", "The 'edit 1' edits an existing record #1." },
+            new string[] { "find", "finds records with a scpecified firstname or lastname.", "The 'find firstname Petr' serches all records with firstname Petr." },
         };
 
         public static void Main(string[] args)
@@ -113,60 +124,237 @@ namespace FileCabinetApp
 
         private static void Create(string parameters)
         {
-            Console.Write("First Name: ");
-            string firsName = Console.ReadLine();
-            NamesInputCheck(firsName, "First Name");
-            Console.Write("Last Name: ");
-            string lastName = Console.ReadLine();
-            NamesInputCheck(lastName, "Last Name");
-            Console.Write("Date of birth: ");
-            string dateOfBirth = Console.ReadLine();
-            var dateTimeBirth = DateOfBirthInputCheck(dateOfBirth);
-            Console.Write("ZIP Code: ");
-            short zipCode;
-            _ = short.TryParse(Console.ReadLine(), out zipCode);
-            Console.Write("City: ");
-            string city = Console.ReadLine();
-            Console.Write("Street: ");
-            string street = Console.ReadLine();
-            Console.Write("Salary: ");
-            decimal salary;
-            _ = decimal.TryParse(Console.ReadLine(), out salary);
-            Console.Write("Gender: ");
-            char gender;
-            _ = char.TryParse(Console.ReadLine(), out gender);
-            Console.WriteLine($"Record #{fileCabinetService.CreateRecord(firsName, lastName, dateTimeBirth, zipCode, city, street, salary, gender)} is created");
+            string firstName = ReadNames("First Name: ");
+            string lastName = ReadNames("Last Name: ");
+            DateTime dateTimeBirth = ReadDate("Date of birth: ");
+            short zipCode = ReadZipCode("ZIP Code: ");
+            string city = ReadNames("City: ");
+            string street = ReadNames("Street: ");
+            decimal salary = ReadSalary("Salary: ");
+            char gender = ReadGender("Gender: ");
+            Console.WriteLine($"Record #{fileCabinetService.CreateRecord(firstName, lastName, dateTimeBirth, zipCode, city, street, salary, gender)} is created");
         }
 
-        private static string NamesInputCheck(string input, string parameterName)
+        private static void Edit(string parameters)
         {
-            while (input.Length == 0)
+            int id;
+            if (!int.TryParse(parameters, out id) || id == 0)
             {
-                Console.WriteLine($"{parameterName} cannot be empty, please try again:");
-                input = Console.ReadLine();
+                Console.WriteLine($"#{id} record is not found.");
+                return;
+            }
+
+            FileCabinetRecord[] list = fileCabinetService.GetRecords();
+            bool isFound = false;
+            foreach (var item in list)
+            {
+                if (item.Id == id)
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"#{id} record is not found.");
+                return;
+            }
+
+            string firstName = ReadNames("First Name: ");
+            string lastName = ReadNames("Last Name: ");
+            DateTime dateTimeBirth = ReadDate("Date of birth: ");
+            short zipCode = ReadZipCode("ZIP Code: ");
+            string city = ReadNames("City: ");
+            string street = ReadNames("Street: ");
+            decimal salary = ReadSalary("Salary: ");
+            char gender = ReadGender("Gender: ");
+            fileCabinetService.EditRecord(id, firstName, lastName, dateTimeBirth, zipCode, city, street, salary, gender);
+            Console.WriteLine($"Record #{id} is updated");
+        }
+
+        private static void Find(string parameters)
+        {
+            var inputs = parameters.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+            const int propertyIndex = 0;
+            string property = inputs[propertyIndex];
+
+            if (string.IsNullOrEmpty(property))
+            {
+                Console.WriteLine(Program.HintMessage);
+                return;
+            }
+
+            int index = Array.FindIndex(properties, 0, properties.Length, i => i.Item1.Equals(property, StringComparison.InvariantCultureIgnoreCase));
+            if (index >= 0)
+            {
+                const int valueIndex = 1;
+                string propertyValue = inputs.Length > 1 ? inputs[valueIndex] : string.Empty;
+                properties[index].Item3(properties[index].Item2, propertyValue);
+            }
+            else
+            {
+                PrintMissedPropertyInfo(property);
+            }
+        }
+
+        private static void PrintMissedPropertyInfo(string property)
+        {
+            Console.WriteLine($"There is no '{property}' property.");
+            Console.WriteLine();
+        }
+
+        private static void ShowByFirstName(string propertyFullName, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                string trimmedValue = value.Trim();
+                FileCabinetRecord[] records = fileCabinetService.FindByFirstName(trimmedValue);
+                CheckRecordsForZeroOrShow(records, propertyFullName, trimmedValue);
+            }
+            else
+            {
+                Console.WriteLine($"'{propertyFullName}' value is empty.");
+            }
+        }
+
+        private static void ShowByLastName(string propertyFullName, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                string trimmedValue = value.Trim();
+                FileCabinetRecord[] records = fileCabinetService.FindByLastName(trimmedValue);
+                CheckRecordsForZeroOrShow(records, propertyFullName, trimmedValue);
+            }
+            else
+            {
+                Console.WriteLine($"'{propertyFullName}' value is empty.");
+            }
+        }
+
+        private static void ShowByDate(string propertyFullName, string value)
+        {
+            DateTime dateOfBirth;
+            if (!string.IsNullOrEmpty(value))
+            {
+                string trimmedValue = value.Trim();
+                if (DateTime.TryParse(trimmedValue, out dateOfBirth))
+                {
+                    FileCabinetRecord[] records = fileCabinetService.FindByDateOfBirth(dateOfBirth);
+                    CheckRecordsForZeroOrShow(records, propertyFullName, value);
+                }
+                else
+                {
+                    Console.WriteLine($"There is no records with '{trimmedValue}' {propertyFullName}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"'{propertyFullName}' value is empty.");
+            }
+        }
+
+        private static string ReadNames(string parameterName)
+        {
+            Console.Write($"{parameterName}");
+            string input = Console.ReadLine().Trim();
+            while (string.IsNullOrWhiteSpace(input) || input.Length < 2 || input.Length > 60)
+            {
+                Console.WriteLine($"{parameterName}minimum number of characters is 2, maximum is 60 and cannot be empty or or contain only space characters please try again:");
+                Console.Write($"{parameterName}");
+                input = Console.ReadLine().Trim();
             }
 
             return input;
         }
 
-        private static DateTime DateOfBirthInputCheck(string input)
+        private static DateTime ReadDate(string parameterName)
         {
-            bool isParced;
+            Console.Write($"{parameterName}");
+            string input = Console.ReadLine();
+            bool isCorrectDate = false;
             DateTime dateTimeBirth;
             do
             {
-                isParced = DateTime.TryParse(input, out dateTimeBirth);
-
-                if (!isParced)
+                if (!DateTime.TryParse(input, out dateTimeBirth))
                 {
                     Console.WriteLine("Invalid format: Date of birth");
                     Console.WriteLine("Correct format is: dd/mm/yyyy");
-                    Console.Write("Please, repeat:");
+                    Console.WriteLine("Please, repeat:");
+                    Console.Write($"{parameterName}");
                     input = Console.ReadLine();
                 }
+                else
+                {
+                    if (dateTimeBirth < new DateTime(1950, 1, 1) || dateTimeBirth > DateTime.Now)
+                    {
+                        Console.WriteLine($"Date of birth should be more than 01-Jan-1950 and not more than {DateTime.Now.ToString("dd-MMM-yyyy", DateTimeFormatInfo.InvariantInfo)}");
+                        Console.WriteLine("Please, repeat input.");
+                        Console.Write($"{parameterName}");
+                        input = Console.ReadLine();
+                    }
+                    else
+                    {
+                        isCorrectDate = true;
+                    }
+                }
             }
-            while (!isParced);
+            while (!isCorrectDate);
             return dateTimeBirth;
+        }
+
+        private static short ReadZipCode(string parameterName)
+        {
+            Console.Write($"{parameterName}");
+            short zipCode = 0;
+            bool isParsed = false;
+            while (!isParsed || zipCode < 0 || zipCode > 9999)
+            {
+                isParsed = short.TryParse(Console.ReadLine().Trim(), out zipCode);
+                if (!isParsed || zipCode < 0 || zipCode > 9999)
+                {
+                    Console.WriteLine($"{parameterName} range is 1..9999 or invalid input. Please repeat input.");
+                    Console.Write($"{parameterName}: ");
+                }
+            }
+
+            return zipCode;
+        }
+
+        private static decimal ReadSalary(string parameterName)
+        {
+            Console.Write($"{parameterName}");
+            decimal salary = 0;
+            bool isParsed = false;
+            while (!isParsed || salary < 0 || salary > 100000)
+            {
+                isParsed = decimal.TryParse(Console.ReadLine().Trim(), out salary);
+                if (!isParsed || salary < 0 || salary > 100000)
+                {
+                    Console.WriteLine($"{parameterName}range is 1..100 000, or invalid input. Please repeat input.");
+                    Console.Write($"{parameterName}");
+                }
+            }
+
+            return salary;
+        }
+
+        private static char ReadGender(string parameterName)
+        {
+            Console.Write($"{parameterName}");
+            char gender = default;
+            bool isParsed = false;
+            while (!isParsed || (gender != 'm' && gender != 'M' && gender != 'f' && gender != 'F'))
+            {
+                isParsed = char.TryParse(Console.ReadLine().Trim(), out gender);
+                if (!isParsed || (gender != 'm' && gender != 'M' && gender != 'f' && gender != 'F'))
+                {
+                    Console.WriteLine($"{parameterName}permissible values are :'m', 'M', 'f', 'F'. Please repeat input.");
+                    Console.Write($"{parameterName}");
+                }
+            }
+
+            return gender;
         }
 
         private static void List(string parameters)
@@ -174,15 +362,32 @@ namespace FileCabinetApp
             FileCabinetRecord[] list = fileCabinetService.GetRecords();
             if (list.Length != 0)
             {
-                foreach (var item in list)
-                {
-                    Console.WriteLine($"#{item.Id}, {item.FirstName}, {item.LastName}, {item.DateOfBirth.ToString("yyyy-MMM-dd", DateTimeFormatInfo.InvariantInfo)}, " +
-                        $"{item.ZipCode}, {item.City}, {item.Street}, {item.Salary}? {item.Gender}");
-                }
+                ShowRecords(list);
             }
             else
             {
                 Console.WriteLine("There is no records in list");
+            }
+        }
+
+        private static void ShowRecords(FileCabinetRecord[] records)
+        {
+            foreach (var item in records)
+            {
+                Console.WriteLine($"#{item.Id}, {item.FirstName}, {item.LastName}, {item.DateOfBirth.ToString("yyyy-MMM-dd", DateTimeFormatInfo.InvariantInfo)}, " +
+                    $"{item.ZipCode}, {item.City}, {item.Street}, {item.Salary}, {item.Gender}");
+            }
+        }
+
+        private static void CheckRecordsForZeroOrShow(FileCabinetRecord[] records, string propertyName, string input)
+        {
+            if (records.Length == 0)
+            {
+                Console.WriteLine($"There is no records with {propertyName} '{input}'");
+            }
+            else
+            {
+                ShowRecords(records);
             }
         }
     }
